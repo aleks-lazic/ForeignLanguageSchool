760ae77281de1148ab3d623cdeb986b2d804e853
clivazpat <clivaz.patrick@icloud.com>
2017-05-04 16:08:07 +0200

    Minor fixes

--- app/src/main/java/ch/hes/foreignlanguageschool/Fragments/TodayFragment.java
+++ app/src/main/java/ch/hes/foreignlanguageschool/Fragments/TodayFragment.java
@@ -30,21 +30,9 @@ import ch.hes.foreignlanguageschool.DB.DBStudent;
 import ch.hes.foreignlanguageschool.DB.DatabaseHelper;
 import ch.hes.foreignlanguageschool.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TodayFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TodayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TodayFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -150,16 +138,10 @@ public class TodayFragment extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
     }
 
     @Override
@@ -233,7 +215,6 @@ public class TodayFragment extends Fragment {
     }
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
